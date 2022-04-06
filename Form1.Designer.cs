9c1e86b5e649c60753d8166137ff0e8e2000b158
Behzad Shakeri <bhzd_shakeri@yahoo.com>
2022-04-06 14:37:34 +0430

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace ChangeBackgroundColorRunTime
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.trackBar3 = new System.Windows.Forms.TrackBar();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Window;
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(159, 146);
+			this.panel1.TabIndex = 0;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.ColumnCount = 3;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 76.09562F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 14.34263F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.163346F));
+			this.tableLayoutPanel1.Controls.Add(this.trackBar1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.trackBar2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.trackBar3, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.label1, 2, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 2, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 2, 2);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(348, 12);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(251, 134);
+			this.tableLayoutPanel1.TabIndex = 2;
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.trackBar1.Location = new System.Drawing.Point(5, 3);
+			this.trackBar1.Maximum = 255;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Size = new System.Drawing.Size(180, 38);
+			this.trackBar1.TabIndex = 0;
+			this.trackBar1.TickFrequency = 10;
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.trackBar2.Location = new System.Drawing.Point(5, 47);
+			this.trackBar2.Maximum = 255;
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Size = new System.Drawing.Size(180, 38);
+			this.trackBar2.TabIndex = 1;
+			this.trackBar2.TickFrequency = 10;
+			// 
+			// trackBar3
+			// 
+			this.trackBar3.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.trackBar3.Location = new System.Drawing.Point(5, 91);
+			this.trackBar3.Maximum = 255;
+			this.trackBar3.Name = "trackBar3";
+			this.trackBar3.Size = new System.Drawing.Size(180, 40);
+			this.trackBar3.TabIndex = 2;
+			this.trackBar3.TickFrequency = 10;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.textBox1.Location = new System.Drawing.Point(194, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(30, 20);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.textBox2.Location = new System.Drawing.Point(194, 56);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(30, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.textBox3.Location = new System.Drawing.Point(194, 101);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(30, 20);
+			this.textBox3.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+			this.label1.Location = new System.Drawing.Point(231, 15);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(16, 13);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "R";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+			this.label2.Location = new System.Drawing.Point(231, 59);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(16, 13);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "G";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+			this.label3.Location = new System.Drawing.Point(231, 104);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(15, 13);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "B";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(611, 348);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.TrackBar trackBar3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ChangeBackgroundColorRunTime.Properties;
+
+namespace ChangeBackgroundColorRunTime
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			
+		}
+		int red1, green1, blue1;
+
+		
+	//قبل از ذخیره ی تنظیمات برنامه
+	private void timer1_Tick(object sender, EventArgs e)
+		{
+			textBox1.Text = Convert.ToString(trackBar1.Value);
+			textBox2.Text = Convert.ToString(trackBar2.Value);
+			textBox3.Text = Convert.ToString(trackBar3.Value);
+
+			
+			red1 = Convert.ToInt32(textBox1.Text);
+			green1 = Convert.ToInt32(textBox2.Text);
+			blue1 = Convert.ToInt32(textBox3.Text);
+
+			this.BackColor = Color.FromArgb(red1, green1, blue1);
+			
+		}
+
+		#region SaveSetting
+		//ذخیره تنظیمات برنامه
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			trackBar1.Value = Properties.Settings.Default.trackBar1;
+			trackBar2.Value = Properties.Settings.Default.trackBar2;
+			trackBar3.Value = Properties.Settings.Default.trackBar3;
+
+			label1.ForeColor = Color.FromArgb(red1 + 255, green1 + 255, blue1 + 255);
+			label2.ForeColor = Color.FromArgb(red1 + 255, green1 + 255, blue1 + 255);
+			label3.ForeColor = Color.FromArgb(red1 + 255, green1 + 255, blue1 + 255);
+		}
+
+		private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+		{
+			//if (red1 == 255)
+			//{
+			//	red1 = 0;
+			//	green1 = 0;
+			//	blue1 = 0;
+			//}
+
+			Properties.Settings.Default.trackBar1 = trackBar1.Value;
+			Properties.Settings.Default.trackBar2 = trackBar2.Value;
+			Properties.Settings.Default.trackBar3 = trackBar3.Value;
+
+			Properties.Settings.Default.Save();
+		}
+		#endregion
+
+
+
+
+	}
+}
